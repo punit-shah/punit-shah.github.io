e53f03ccffd9ae4bda8079407711495b62655f89
Punit Shah <punit.shah@bbc.co.uk>
2019-02-13 23:41:11 +0000

    Cover tweaks

--- src/Cover.js
+++ src/Cover.js
@@ -5,7 +5,7 @@ import './Cover.css';
 const Cover = () => (
   <section className="Cover" id="cover">
     <div className="Cover-container container">
-      <h1 className="Cover-header">{'{punit shah}'}</h1>
+      <h1 className="Cover-header">Punit Shah</h1>
       <p className="Cover-lead">Software engineer</p>
       <hr className="Cover-line" />
       <Error />
