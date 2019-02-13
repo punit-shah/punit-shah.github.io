02db7345864ac90ed297e62f448e6a136a67e363
Punit Shah <punit.shah@bbc.co.uk>
2019-02-13 15:35:37 +0000

    Add cover section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Cover from './Cover';
 import './App.css';
 
-const links = [
-  { href: '#work', label: 'work', onClick: () => console.log('scroll to #work') },
-  { href: '#about', label: 'about', onClick: () => console.log('scroll to #about') },
-  { href: '#contact', label: 'contact', onClick: () => console.log('scroll to #contact') },
+const navbarLinks = [
+  {
+    href: '#work',
+    label: 'work',
+    onClick: () => console.log('scroll to #work'),
+  },
+  {
+    href: '#about',
+    label: 'about',
+    onClick: () => console.log('scroll to #about'),
+  },
+  {
+    href: '#contact',
+    label: 'contact',
+    onClick: () => console.log('scroll to #contact'),
+  },
 ];
 
 const App = () => (
   <div className="App">
-    <Navbar fixed links={links} />
+    <Navbar fixed links={navbarLinks} />
+    <Cover />
   </div>
 );
 
---
+++ src/Cover.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Cover.css';
+
+const Cover = () => (
+  <section className="Cover" id="cover">
+    <div className="Cover-container container">
+      <h1 className="Cover-header">{'{punit shah}'}</h1>
+      <p className="Cover-lead">Software engineer</p>
+      <hr className="Cover-line" />
+    </div>
+  </section>
+);
+
+export default Cover;
