28023a43066d642259d5e01072b06cf409fe1556
Punit Shah <punit.shah@bbc.co.uk>
2019-02-14 00:46:09 +0000

    Refactor Error

--- src/Error.js
+++ src/Error.js
@@ -5,18 +5,18 @@ import errorImg from './img/error.png';
 
 const getErrorMessage = () => {
   const messages = [
-    '01101001 01101100 01111001',
-    'you need more ram',
-    'please do not touch me',
-    'a long time ago, clicking on me would have crashed your browser',
-    "back in my day we didn't click on errors for no reason",
-    'i am not an a.i.',
-    '<marquee>i miss this</marquee>',
-    'why was i built with react',
     'this site needs an "under construction" gif',
+    'please do not touch me',
+    "back in my day we didn't click on errors for no reason",
+    'a long time ago, clicking on me would have crashed your browser',
+    'i am not an a.i.',
+    'why was i built with react',
+    '<marquee>i miss this</marquee>',
+    'you need more ram',
   ];
+  const randomMessageIndex = Math.floor(Math.random() * messages.length);
 
-  return messages[Math.floor(Math.random() * messages.length)];
+  return messages[randomMessageIndex];
 };
 
 const Error = () => {
@@ -32,34 +32,30 @@ const Error = () => {
   );
 
   const bubble = (
-    <div className={bubbleClasses} aria-hidden={isBubbleHidden}>
+    <div className={bubbleClasses} aria-hidden>
       <div className="Error-bubble-message">
         <span>{errorMessage}</span>
       </div>
     </div>
   );
 
+  const handleClick = () => {
+    if (bubbleTimeout) {
+      window.clearTimeout(bubbleTimeout);
+    }
+
+    const message = getErrorMessage();
+    const delay = message.includes('<marquee>') ? 5000 : 1000;
+
+    setErrorMessage(message);
+    setIsBubbleHidden(false);
+    setBubbleTimeout(window.setTimeout(() => setIsBubbleHidden(true), delay));
+  };
+
   return (
     <figure className="Error">
       <div className="Error-image">
-        <img
-          src={errorImg}
-          alt="Error: 🌚"
-          onClick={() => {
-            if (bubbleTimeout) {
-              window.clearTimeout(bubbleTimeout);
-            }
-
-            const message = getErrorMessage();
-            const delay = message.includes('<marquee>') ? 5000 : 1000;
-
-            setErrorMessage(message);
-            setIsBubbleHidden(false);
-            setBubbleTimeout(
-              window.setTimeout(() => setIsBubbleHidden(true), delay)
-            );
-          }}
-        />
+        <img src={errorImg} alt="Error: 🌚" onClick={handleClick} />
         {bubble}
       </div>
       <figcaption className="Error-caption">
