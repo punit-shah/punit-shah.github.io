096618f20a829ccd87b7fc759c3c9b1049ed7b3b
Punit Shah <punit.shah@bbc.co.uk>
2019-07-28 19:06:09 +0100

    Add iplayer work item

--- src/components/WorkItem.js
+++ src/components/WorkItem.js
@@ -9,23 +9,33 @@ const WorkItem = ({
     description,
     links: { visit, source },
   },
-}) => (
-  <article className="WorkItem">
-    <div className="WorkItem-image">
-      <img src={image} alt={`Screenshot of ${title}`} />
-    </div>
-    <h3 className="WorkItem-title">{title}</h3>
-    <p className="WorkItem-description">{description}</p>
-    <ul className="WorkItem-links">
-      <li className="WorkItem-link WorkItem-link-visit">
-        <a href={visit}>Visit</a>
-      </li>
-      <li className="WorkItem-link WorkItem-link-source">
-        <a href={source}>Source</a>
-      </li>
-    </ul>
-  </article>
-);
+}) => {
+  const hasLinks = visit || source;
+
+  return (
+    <article className="WorkItem">
+      <div className="WorkItem-image">
+        <img src={image} alt={`Screenshot of ${title}`} />
+      </div>
+      <h3 className="WorkItem-title">{title}</h3>
+      <p className="WorkItem-description">{description}</p>
+      {hasLinks && (
+        <ul className="WorkItem-links">
+          {visit && (
+            <li className="WorkItem-link WorkItem-link-visit">
+              <a href={visit}>Visit</a>
+            </li>
+          )}
+          {source && (
+            <li className="WorkItem-link WorkItem-link-source">
+              <a href={source}>Source</a>
+            </li>
+          )}
+        </ul>
+      )}
+    </article>
+  );
+};
 
 WorkItem.propTypes = {
   data: PropTypes.shape({
@@ -33,9 +43,9 @@ WorkItem.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     links: PropTypes.shape({
-      visit: PropTypes.string.isRequired,
-      source: PropTypes.string.isRequired,
-    }).isRequired,
+      visit: PropTypes.string,
+      source: PropTypes.string,
+    }),
   }).isRequired,
 };
 
--- src/sections/Work.js
+++ src/sections/Work.js
@@ -3,6 +3,7 @@ import './Work.css';
 import asteroids from '../img/asteroids.png';
 import realTimeFaceAging from '../img/real-time-face-aging.png';
 import cssSpecificityCalculator from '../img/css-specificity-calculator.png';
+import iplayer from '../img/iplayer.png';
 import WorkItem from '../components/WorkItem';
 
 const items = [
@@ -27,22 +28,21 @@ const items = [
     },
   },
   {
+    image: iplayer,
+    title: 'BBC iPlayer',
+    description:
+      'Helped maintain and develop new features for the BBC iPlayer website',
+    links: {
+      visit: 'https://www.bbc.co.uk/iplayer',
+    },
+  },
+  {
     image: cssSpecificityCalculator,
     title: 'CSS specificity calculator',
     description: 'A tool to help developers understand CSS specificity',
     links: {
       visit: 'https://punit-shah.github.io/specificity/',
       source: 'https://github.com/punit-shah/specificity',
-    },
-  },
-  {
-    image: asteroids,
-    title: 'Asteroids',
-    description:
-      'A clone of the classic arcade game Asteroids in JavaScript 🚀👾',
-    links: {
-      visit: 'https://punit-shah.github.io/asteroids/',
-      source: 'https://github.com/punit-shah/asteroids',
     },
   },
 ];
