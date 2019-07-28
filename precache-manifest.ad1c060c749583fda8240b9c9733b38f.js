91afe1f94513ad763ffa87202a37eb051f16c183
Punit Shah <punit.shah@bbc.co.uk>
2019-07-28 20:49:40 +0100

    Updates

---
+++ precache-manifest.ad1c060c749583fda8240b9c9733b38f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f007c87d9dd095245b0f90997dc585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fa1595a0f908c5ddac8",
+    "url": "/static/css/main.75fa5a7a.chunk.css"
+  },
+  {
+    "revision": "850f7e9a860e70a5898f",
+    "url": "/static/js/2.77d18c78.chunk.js"
+  },
+  {
+    "revision": "0fa1595a0f908c5ddac8",
+    "url": "/static/js/main.17d49182.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "be7d6f96567793979cd737590e5614f3",
+    "url": "/static/media/asteroids.be7d6f96.png"
+  },
+  {
+    "revision": "ebe91587cc0cc32e5bc1d8ace466f1bf",
+    "url": "/static/media/climate-change.ebe91587.png"
+  },
+  {
+    "revision": "cd6b38f5ffb96d58464cd3662d9585eb",
+    "url": "/static/media/css-specificity-calculator.cd6b38f5.png"
+  },
+  {
+    "revision": "6961979a193368576aa2f122fbc86ee4",
+    "url": "/static/media/google-labour-leadership.6961979a.png"
+  },
+  {
+    "revision": "3bb23e3cc9ac869077f6d10f304d32da",
+    "url": "/static/media/iplayer.3bb23e3c.png"
+  },
+  {
+    "revision": "ed873a7702eff6f77425cdc3de39e7de",
+    "url": "/static/media/musical-twin-towns.ed873a77.png"
+  },
+  {
+    "revision": "9c49e25839c0a000388f9e496b8d7adb",
+    "url": "/static/media/real-time-face-aging.9c49e258.png"
+  },
+  {
+    "revision": "c6e7cb1ea245bb570ab8ea150dcd452d",
+    "url": "/static/media/syria.c6e7cb1e.png"
+  }
+]);
