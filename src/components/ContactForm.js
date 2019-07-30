df0e3dd1e53b03b37ab1b4a7176d4ab9babebe3b
Punit Shah <punit.shah@bbc.co.uk>
2019-07-30 04:13:24 +0100

    ContactForm component with state, using contact-form-api

---
+++ src/components/ContactForm.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import Button from './Button';
+import './ContactForm.css';
+
+const formApiUrl =
+  'https://xlframabl3.execute-api.eu-west-1.amazonaws.com/prod/email/send';
+
+const postData = async (url, data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+};
+
+const ContactForm = () => {
+  const [formState, setFormState] = useState('idle');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const messages = {
+    idle: '\u00A0',
+    sending: 'Sending...',
+    success: 'Thanks for the message!',
+    failure: 'Oops, something went wrong! Please try sending that again later.',
+  };
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    setFormState('sending');
+
+    try {
+      await postData(formApiUrl, {
+        name,
+        email,
+        message,
+      });
+
+      setFormState('success');
+      setName('');
+      setEmail('');
+      setMessage('');
+
+      setTimeout(() => setFormState('idle'), 5000);
+    } catch (error) {
+      console.error(error);
+      setFormState('failure');
+
+      setTimeout(() => setFormState('idle'), 5000);
+    }
+  };
+
+  return (
+    <form className="ContactForm" onSubmit={onSubmit}>
+      <p>
+        Got a question? I'd love to hear from you. Send me a message and I'll
+        respond as soon as possible.
+      </p>
+      <label className="ContactForm-label">
+        Name
+        <input
+          className="ContactForm-field"
+          type="text"
+          name="name"
+          value={name}
+          onChange={event => setName(event.target.value)}
+          required
+        />
+      </label>
+      <label className="ContactForm-label">
+        Email address
+        <input
+          className="ContactForm-field"
+          type="email"
+          name="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+          required
+        />
+      </label>
+      <label className="ContactForm-label">
+        Message
+        <textarea
+          className="ContactForm-body"
+          type="text"
+          name="message"
+          value={message}
+          onChange={event => setMessage(event.target.value)}
+          required
+        />
+      </label>
+      <Button
+        className="ContactForm-submit"
+        type="submit"
+        value="🚀 Send message"
+        disabled={formState === 'sending'}
+      />
+      {messages[formState] && (
+        <p className={`ContactForm-message ContactForm-message-${formState}`}>
+          {messages[formState]}
+        </p>
+      )}
+    </form>
+  );
+};
+
+export default ContactForm;
--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,60 +1,13 @@
 import React, { forwardRef } from 'react';
 import SectionHeading from '../components/SectionHeading';
-import Button from '../components/Button';
+import ContactForm from '../components/ContactForm';
 import './Contact.css';
 
 const Contact = forwardRef((props, ref) => (
   <section className="Contact" id="contact" ref={ref}>
     <div className="Contact-container container">
       <SectionHeading className="Contact-title">Contact</SectionHeading>
-      <form
-        className="Contact-form"
-        action="https://formspree.io/punitshah95@gmail.com"
-        method="POST"
-      >
-        <p>
-          Got a question? I'd love to hear from you. Send me a message and I'll
-          respond as soon as possible.
-        </p>
-        <label className="Contact-label">
-          Name
-          <input
-            className="Contact-field"
-            id="Contact-name"
-            type="text"
-            name="name"
-            required
-          />
-        </label>
-        <label className="Contact-label">
-          Email address
-          <input
-            className="Contact-field"
-            id="Contact-email"
-            type="email"
-            name="_replyto"
-            required
-          />
-        </label>
-        <label className="Contact-label">
-          Message
-          <textarea
-            className="Contact-body"
-            id="Contact-message"
-            type="text"
-            name="message"
-            required
-          />
-        </label>
-        <input className="Contact-hidden" type="text" name="_gotcha" />
-        <input type="hidden" name="_subject" value="Contact form message" />
-        <Button
-          className="Contact-submit"
-          id="Contact-send"
-          type="submit"
-          value="🚀 Send message"
-        />
-      </form>
+      <ContactForm />
     </div>
   </section>
 ));
